73af5c1ef2ab2dda41e89e257c91127060119de0
Aidan Link <AidanLink@u.boisestate.edu>
2025-02-20 16:07:28 -0700

    Added Camera Shake

--- Assets/Code/CamerFollow.cs
+++ Assets/Code/CamerFollow.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class FollowCamera : MonoBehaviour
 {
-    public Transform target; 
-    public Vector3 offset = new Vector3(0, 10, -10); 
-    public float smoothSpeed = 5f; 
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 10, -10);
+    public float smoothSpeed = 5f;
 
     void LateUpdate()
     {
@@ -12,5 +14,26 @@ public class FollowCamera : MonoBehaviour
 
         Vector3 desiredPosition = target.position + offset;
         transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+    }
+
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPosition = transform.localPosition;
+
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = originalPosition + new Vector3(x, y, 0);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPosition;
     }
 }
