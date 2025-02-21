60652a2c94d582b123e3d1fb9112264f2367da51
C1aphusCa1n <ecline1206@icloud.com>
2025-02-20 23:19:31 -0700

    organized my scripts a bit
    pener bener

---
+++ Assets/Code/Power Ups/Power_Up_Respawner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Power_Up_Respawner : MonoBehaviour
+{
+    //respawn the power up script at its exact position it was before it was destroyed
+   
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
